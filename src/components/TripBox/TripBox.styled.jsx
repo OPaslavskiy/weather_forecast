7e9be6e2db2cd1d09cbe320563df7ead2e359f73
OPaslavskiy <bankir.op@gmail.com>
2023-07-18 13:18:42 +0300

    add syle

--- src/components/TripBox/TripBox.styled.jsx
+++ src/components/TripBox/TripBox.styled.jsx
@@ -3,5 +3,22 @@ import styled from "styled-components";
 export const TripList = styled.ul`
   display: flex;
   gap: 40px;
-  overflow: auto;
+  overflow-x: scroll;
+  padding-bottom: 10px;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+    height: 6px;
+    border-radius: 8px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 8px;
+    background-color: #bebebe;
+    transition: background-color 0.4s cubic-bezier(0.4, 0, 0.2, 1);
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: #919191;
+  }
 `;
--- src/components/WeatherTimeBox/WeatherTimeBox.styled.jsx
+++ src/components/WeatherTimeBox/WeatherTimeBox.styled.jsx
@@ -5,4 +5,21 @@ export const ListWeather = styled.ul`
   gap: 40px;
   margin-top: 70px;
   overflow: auto;
+  padding-bottom: 10px;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+    height: 6px;
+    border-radius: 8px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 8px;
+    background-color: #bebebe;
+    transition: background-color 0.4s cubic-bezier(0.4, 0, 0.2, 1);
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: #919191;
+  }
 `;
