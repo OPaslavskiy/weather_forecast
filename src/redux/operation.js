8b128bd43ee8f8f54b673c90ebe66f226e013ad4
OPaslavskiy <bankir.op@gmail.com>
2023-07-19 14:44:56 +0300

    add key

--- src/redux/operation.js
+++ src/redux/operation.js
@@ -1,8 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-const API_KEY_WEATHER = import.meta.env.VITE_API_KEY;
 
-const API_KEY = API_KEY_WEATHER;
+const API_KEY = "8KUAFTVQBADSY7D8SJM6LWT94";
 
 axios.defaults.baseURL =
   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
