10e32a5e96d0bb1d5244c32a871ee160b9358bf3
OPaslavskiy <bankir.op@gmail.com>
2023-07-19 16:05:43 +0300

    add if NaN

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -6,7 +6,7 @@ import { calculateTime } from "../../services";
 const Timer = () => {
   const startDate = useSelector((state) => state.startDate);
   const [timeLeft, setTimeLeft] = useState(calculateTime());
-
+  console.log(startDate);
   useEffect(() => {
     const timer = setInterval(() => {
       setTimeLeft(calculateTime(startDate));
@@ -16,24 +16,28 @@ const Timer = () => {
   }, [startDate]);
 
   return (
-    <TimerBox>
-      <TimeBox>
-        <Value>{timeLeft.days}</Value>
-        <Indicator>DAYS</Indicator>
-      </TimeBox>
-      <TimeBox>
-        <Value>{timeLeft.hours}</Value>
-        <Indicator>HOURS</Indicator>
-      </TimeBox>
-      <TimeBox>
-        <Value>{timeLeft.minutes}</Value>
-        <Indicator>MINUTES</Indicator>
-      </TimeBox>
-      <TimeBox>
-        <Value>{timeLeft.seconds}</Value>
-        <Indicator>SECONDS</Indicator>
-      </TimeBox>
-    </TimerBox>
+    <>
+      {!isNaN(timeLeft.days) && (
+        <TimerBox>
+          <TimeBox>
+            <Value>{timeLeft.days}</Value>
+            <Indicator>DAYS</Indicator>
+          </TimeBox>
+          <TimeBox>
+            <Value>{timeLeft.hours}</Value>
+            <Indicator>HOURS</Indicator>
+          </TimeBox>
+          <TimeBox>
+            <Value>{timeLeft.minutes}</Value>
+            <Indicator>MINUTES</Indicator>
+          </TimeBox>
+          <TimeBox>
+            <Value>{timeLeft.seconds}</Value>
+            <Indicator>SECONDS</Indicator>
+          </TimeBox>
+        </TimerBox>
+      )}
+    </>
   );
 };
 
