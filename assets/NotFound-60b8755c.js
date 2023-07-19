4ab3ca89f07681f9c2f421e68cfa769153f6cf85
OPaslavskiy <OPaslavskiy@users.noreply.github.com>
2023-07-19 12:37:07 +0000

    deploy: 05396a397950eb6a0bd81f53f4c2d365a93e4e04

---
+++ assets/NotFound-60b8755c.js
@@ -0,0 +1,34 @@
+import{s as e,L as n,j as t}from"./index-75c3c242.js";const s=e.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 5%;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: space-around;
+`,i=e.h1`
+  color: red;
+  font-size: 100px;
+  margin-bottom: 40px;
+  font-size: 50px;
+`,c=e.h2`
+  font-size: 50px;
+  margin-bottom: 40px;
+  font-size: 25px;
+`,r=e.h3`
+  margin-bottom: 40px;
+`,a=e.div`
+  height: 50vh;
+`,h=e(n)`
+  text-decoration: none;
+  margin-right: 20px;
+
+  &:hover,
+  &:focus {
+    color: red;
+    text-decoration: underline;
+  }
+`,o=e.img`
+  width: 30vw;
+  height: auto;
+`,d=()=>t.jsx("div",{children:t.jsxs(s,{children:[t.jsxs("div",{children:[t.jsx(o,{src:"https://assets.codepen.io/5647096/backToTheHomepage.png",alt:"Back to the Homepage"}),t.jsx(o,{src:"https://assets.codepen.io/5647096/Delorean.png",alt:"El Delorean, El Doc y Marti McFly"})]}),t.jsxs(a,{children:[t.jsx(i,{children:"404"}),t.jsx(c,{children:"PAGE NOT FOUND"}),t.jsx(r,{children:"BACK TO HOME?"}),t.jsx(h,{to:"/",children:"YES"})]})]})});export{d as default};
